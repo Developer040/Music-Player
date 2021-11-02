4298f7a1b404bf0f532c76906be7d344ac8a35d4
Developer <mayubxon2003@gmail.com>
2021-11-02 18:04:38 +0300

    Bu music player ilovasi

---
+++ app/src/main/java/com/developer/mediaplayer/Models/music.kt
@@ -0,0 +1,6 @@
+package com.developer.mediaplayer.Models
+
+
+import java.io.Serializable
+
+data class Music(val id:Long, val title:String, val imagePath:String, val musicPath:String, val author:String):Serializable
